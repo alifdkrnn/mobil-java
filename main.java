2f5b4831a43da9950de9ca4c7d2d3f38b60f1a05
alifdkrnn <129760383+alifdkrnn@users.noreply.github.com>
2023-06-13 07:26:58 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,30 @@
+import barang.Mobil;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        PenjualanMobil penjualanMobil = new PenjualanMobil();
+
+        // Tambah mobil ke dalam daftar
+        Mobil mobil1 = new Mobil("Toyota", "Avanza", 2021, 150000000);
+        penjualanMobil.beliMobil(mobil1);
+
+        Mobil mobil2 = new Mobil("Honda", "Civic", 2022, 200000000);
+        penjualanMobil.beliMobil(mobil2);
+
+        // Jual mobil dengan harga jual tertentu
+        double hargaJualMobil2 = 210000000;
+        penjualanMobil.jualMobil(mobil2, hargaJualMobil2);
+
+        // Output nilai akhir modal dan aset
+        System.out.println("Nilai Akhir Modal: " + penjualanMobil.getUangModal());
+        System.out.println("Nilai Akhir Aset: " + penjualanMobil.getNilaiAsset());
+
+        // Tampilkan daftar mobil dengan menampilkan modelnya
+        List<Mobil> daftarMobil = penjualanMobil.getDaftarMobil();
+        System.out.println("Daftar Mobil:");
+        for (Mobil mobil : daftarMobil) {
+            System.out.println("Model: " + mobil.getModel());
+        }
+    }
+}
